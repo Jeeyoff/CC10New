df59f409b8a21b78f3672fd32d3572e2d0183cd1
Jeeyoff <sorplopia@gmail.com>
2024-11-13 15:57:36 +0700

    Turns out I do need it.

--- logscript.js
+++ logscript.js
@@ -75,3 +75,26 @@ signUp.addEventListener("click", (event) => {
     });
 });*/
 
+const signIn = document.getElementById("login");
+signIn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("pass").value;
+  const auth = getAuth();
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      showMessage("Login Successful", "signInMessage");
+      const user = userCredential.user;
+      localStorage.setItem("loggedInUserId", user.uid);
+      window.location.href = "homepage.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode === "auth/invalid-credential") {
+        showMessage("Incorrect Email or Password", "signInMessage");
+      } else {
+        showMessage("Account does not Exist");
+      }
+    });
+});
