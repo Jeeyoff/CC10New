90c2408da717a169a3697c941dc2ca7176a2f2d8
Jeeyoff <sorplopia@gmail.com>
2024-11-14 13:51:15 +0700

    Removed obsolete jscript and added two new files.

---
+++ register.js
@@ -0,0 +1,100 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import {
+  getFirestore,
+  setDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDTRDhb5JDqjKJz7SYL0mO_1FVkEGtfm3Q",
+    authDomain: "cc10-3670c.firebaseapp.com",
+    projectId: "cc10-3670c",
+    storageBucket: "cc10-3670c.appspot.com",
+    messagingSenderId: "830759887614",
+    appId: "1:830759887614:web:8bc94abc64be12aefdfbaf",
+    measurementId: "G-NL1SVC3LBF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+function showMessage(message, divId) {
+  var messageDiv = document.getElementById(divId);
+  messageDiv.style.display = "block";
+  messageDiv.innerHTML = message;
+  messageDiv.style.opacity = 1;
+  setTimeout(function () {
+    messageDiv.style.opacity = 0;
+  }, 5000);
+}
+
+const signUp = document.getElementById("subreg");
+signUp.addEventListener("click", (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const PNumber = document.getElementById("PNumber").value;
+  const password = document.getElementById("pass").value;
+
+  const auth = getAuth();
+  const db = getFirestore();
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      const userData = {
+        email: email,
+        username: username,
+        PNumber: PNumber,
+      };
+      showMessage("Account Created Successfully", "signUpMessage");
+      const docRef = doc(db, "user", user.uid);
+      setDoc(docRef, userData)
+        .then(() => {
+          window.location.href = "index.html";
+        })
+        .catch((error) => {
+          console.error("Error writing doc", error);
+        });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode == "auth/email-already-in-use") {
+        showMessage("Email Address already Exists!!!", "signUpMessage");
+      } else {
+        showMessage("Unable to create user", "signUpMessage");
+      }
+    });
+});
+
+/*const signIn = document.getElementById("login");
+signIn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("pass").value;
+  const auth = getAuth();
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      showMessage("Login Successful", "signInMessage");
+      const user = userCredential.user;
+      localStorage.setItem("loggedInUserId", user.uid);
+      window.location.href = "homepage.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      if (errorCode === "auth/invalid-credential") {
+        showMessage("Incorrect Email or Password", "signInMessage");
+      } else {
+        showMessage("Account does not Exist");
+      }
+    });
+});*/
